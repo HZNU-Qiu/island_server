1bf8e5a3c2b970cf39d7e087af64f67f6d5a5482
kingjade <18196777756@163.com>
2021-03-02 22:10:20 +0800

    教师题库模块完成

---
+++ app/api/web/exercise.js
@@ -0,0 +1,43 @@
+const Router = require('koa-router')
+const router = new Router({ prefix: '/web/exercise' })
+const { Exercise } = require('../../models/exercise')
+const { success } = require('../../lib/helper')
+const { Auth } = require('../../../middlewares/auth')
+
+router.post('/add', new Auth(8).m, async (ctx) => {
+  let data = ctx.request.body
+  let creatorId = ctx.auth.uid
+  data.creatorId = creatorId
+  await Exercise.add(data)
+  success('ok')
+})
+
+router.post('/modify', new Auth(8).m, async (ctx) => {
+  let data = ctx.request.body
+  await Exercise.modify(data)
+  success('ok')
+})
+
+router.post('/getByConditions', new Auth(4).m, async (ctx) => {
+  let condition = ctx.request.body
+  let creatorId = ctx.auth.uid
+  condition.creatorId = creatorId
+  let res = await Exercise.getExercises(condition)
+  res.map((item) => {
+    if (item.options !== null && item.options !== "")
+      item.options = item.options.split(';')
+  })
+  let data = {'total': 0, 'list': []}
+  data.list = res
+  if (res.length !== 0)
+    data.total = res[0].total
+  success('ok', data)
+})
+
+router.get('/detail/:id', new Auth(8).m, async (ctx) => {
+  let id = ctx.params.id
+  let data = await Exercise.getOne(id)
+  success('ok', data)
+})
+
+module.exports = router
--- app/models/exercise.js
+++ app/models/exercise.js
@@ -2,7 +2,80 @@ const { db } = require('../../core/db')
 const { Sequelize, Model } = require('sequelize')
 
 class Exercise extends Model {
+  /**
+   * 新增理论练习题(发布/草稿)
+   */
+  static async add(data) {
+    await Exercise.create({
+      ...data
+    })
+  }
 
+  /**
+   * 条件检索理论题库 flag
+   * directionId flag-1
+   * categoryId flag-2
+   * difficulty flag-3
+   * type flag-4
+   * filter 1-all 2-status=1,creatorId 3-status=0,creatorId
+   */
+  static async getExercises(condition) {
+    let query = []
+    if (condition.directionId !== -1) {
+      query.push(`e.direction_id = ${condition.directionId}`)
+    }
+    if (condition.categoryId !== -1) {
+      query.push(`e.category_id = ${condition.categoryId}`)
+    }
+    if (condition.difficulty !== -1) {
+      query.push(`e.difficulty = ${condition.difficulty}`)
+    }
+    if (condition.type !== -1) {
+      query.push(`e.type = ${condition.type}`)
+    }
+    if (condition.filter === 2) {
+      query.push(`e.status = 1 AND e.creator_id = ${condition.creatorId}`)
+    } else if (condition.filter === 3) {
+      query.push(`e.status = 0 AND e.creator_id = ${condition.creatorId}`)
+    } else {
+      query.push(`e.status = 1`)
+    }
+    let str = query.join(' AND ')
+    let sql = `SELECT e.*, d.name AS directionName, c.name AS categoryName, u.realname AS userName,
+    (SELECT COUNT(1) FROM exercise e WHERE ${str}) AS total
+    FROM exercise e JOIN direction d ON e.direction_id = d.id
+    JOIN category c ON c.id = e.category_id
+    JOIN user u ON u.id = e.creator_id
+    GROUP BY e.id
+    HAVING `
+    sql += str + ` limit 10 offset ${(condition.current - 1) * 10}`
+    let data = await db.query(sql, { raw: true })
+    return data[0]
+  }
+
+  /**
+   * 根据ID获取题目详情
+   */
+  static async getOne(id) {
+    return await Exercise.findOne({
+      where: {
+        id
+      }
+    })
+  }
+
+  /**
+   * 编辑题目
+   */
+  static async modify(data) {
+    await Exercise.update({
+      ...data
+    }, {
+      where: {
+        id: data.id
+      }
+    })
+  }
 }
 
 Exercise.init({
@@ -24,6 +97,8 @@ Exercise.init({
   categoryId: Sequelize.INTEGER,
   // 题干信息
   content: Sequelize.TEXT,
+  // 选项信息 用;分割
+  options: Sequelize.TEXT,
   // 正确答案
   answer: Sequelize.STRING(255),
   // 提示
